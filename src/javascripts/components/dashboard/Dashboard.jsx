ff6c8467e3153838d6d686c93c12be35f5c30dea
Vanessa <vanessa@MacBook-Pro-de-Vanessa.local>
2017-06-21 02:06:03 -0300

    create Widgets components and some layout tests

--- src/javascripts/components/dashboard/Dashboard.jsx
+++ src/javascripts/components/dashboard/Dashboard.jsx
@@ -1,9 +1,6 @@
 import React, { Component } from 'react';
 import { Provider, connect } from 'react-redux';
 
-import { Row, Col } from 'react-bootstrap';
-import { Icon } from 'react-fa'
-
 import PropTypes from 'prop-types';
 import { dashboardShape } from './shapes';
 
@@ -12,6 +9,8 @@ import * as actions from '../reducers/dashboard';
 
 import logo from '../../../images/logo.png';
 import '../../../stylesheets/Dashboard.css';
+
+import Widgets from './widgets/Widgets';
 
 class Dashboard extends Component {
   static propTypes = {
@@ -35,74 +34,13 @@ class Dashboard extends Component {
       <div>
         <div className="Dashboard">
           <div className="Dashboard-header">
-            <img src={ logo }/>
+            <img src={ logo } alt='logo'/>
           </div>
 
           <div className="Dashboard-content">
 
             <h1>Dashboard</h1>
-            <Row>
-              <Col lg={3} md={3} sm={6} xs={12} >
-                <div className='Dashboard-widgets'> 
-                  <Row>
-                    <Col md={4} className='Dashboard-widgets-icon-1'>
-                      <Icon name="shopping-bag" size='3x' inverse/>
-                    </Col>
-                    <Col md={8} className='Dashboard-widgets-number'>
-                      <h3>
-                        <span>{ widgets.newOrders }</span>
-                      </h3>
-                      <small>New Orders</small>
-                    </Col>
-                  </Row>
-                </div>
-              </Col>
-              <Col lg={3} md={3} sm={6} xs={12} >
-                <div className='Dashboard-widgets'>
-                  <Row>
-                    <Col md={4} className='Dashboard-widgets-icon-2'>
-                      <Icon name="comment-o" size='3x' inverse/>
-                    </Col>
-                    <Col md={8} className='Dashboard-widgets-number'>
-                      <h3>
-                        <span>{ widgets.comments }</span>
-                      </h3>
-                      <small>Comments</small>
-                    </Col>
-                  </Row>
-                </div>
-              </Col>
-              <Col lg={3} md={3} sm={6} xs={12} >
-                <div className='Dashboard-widgets'>
-                  <Row>
-                    <Col md={4} className='Dashboard-widgets-icon-3'>
-                      <Icon name="user-o" size='3x' inverse/>
-                    </Col>
-                    <Col md={8} className='Dashboard-widgets-number'>
-                      <h3>
-                        <span>{ widgets.newUsers }</span>
-                      </h3>
-                      <small>New Users</small>
-                    </Col>
-                  </Row>
-                </div>
-              </Col>
-              <Col lg={3} md={3} sm={6} xs={12} >
-                <div className='Dashboard-widgets'>
-                  <Row>
-                    <Col md={4} className='Dashboard-widgets-icon-4'>
-                      <Icon name="drivers-license-o" size='3x' inverse/>
-                    </Col>
-                    <Col md={8} className='Dashboard-widgets-number'>
-                      <h3>
-                        <span>{ widgets.newUsers }</span>
-                      </h3>
-                      <small>Page views</small>
-                    </Col>
-                  </Row>
-                </div>
-              </Col>
-            </Row>
+            <Widgets widgets={ widgets  }/>
           </div>
         </div>
       </div> 
@@ -114,4 +52,6 @@ const mapStateToProps = (state) => { return state.dashboard.toJS(); };
 function mapDispatchToProps(dispatch) { return { ...actions, dispatch }; } 
 const DashboardContainer = connect(mapStateToProps, mapDispatchToProps)(Dashboard); 
 
-export default function Container(props) { return <Provider store={store}><DashboardContainer {...props}/></Provider>; }
+export default function Container(props) { 
+  return <Provider store={store}><DashboardContainer {...props}/></Provider>; 
+}
